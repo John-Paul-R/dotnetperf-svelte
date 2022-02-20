a57fa207a2158def2c7d521bfbc4132e0cf87549
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-02-20 01:09:36 -0500

    Add 'relative' chart display mode.

--- src/BenchBarChart.svelte
+++ src/BenchBarChart.svelte
@@ -43,31 +43,62 @@ const benchMeanTimes = rows
         return accum;
     }, {} as { [key: string]: number[] });
 
-const maxItems =
+const maxItems: c3.PrimitiveArray =
     MaxItemsIdx != -1
         ? [...new Set(cols[MaxItemsIdx].slice(1))]
-            .map(Number)
-            .sort((a, b) => a - b)
+              .map(Number)
+              .sort((a, b) => a - b)
         : ['All'];
 
-const dataCols = [
+const buildDataCols = () => {
+    const maxVal = Math.max(
+        ...Object.values(benchMeanTimes).flatMap((arr) => arr)
+    );
+
+    const data = [
+        ...Object.entries(benchMeanTimes).map<[string, ...number[]]>(
+            ([benchName, benchData]) => [benchName, ...benchData]
+        ),
+    ];
+    console.log('one', data);
+    for (let i = 1; i < data[0].length; i++) {
+        let maxInSet = -1;
+        for (let j = 0; j < data.length; j++) {
+            if (data[j][i] > maxInSet) {
+                maxInSet = data[j][i] as number;
+            }
+        }
+        const multiplier = maxVal / maxInSet;
+        for (let j = 0; j < data.length; j++) {
+            data[j][i] = ((data[j][i] as number) * multiplier) / maxVal; // out of 1
+        }
+    }
+    console.log('two', data);
+
+    return [['x', ...maxItems], ...data];
+};
+
+const dataCols: [string, ...c3.PrimitiveArray][] = [
     ['x', ...maxItems],
-    ...Object.entries(benchMeanTimes).map(([benchName, benchData]) => [
-        benchName,
-        ...benchData,
-    ]),
+    ...Object.entries(benchMeanTimes).map<[string, ...number[]]>(
+        ([benchName, benchData]) => [benchName, ...benchData]
+    ),
 ];
+
+let dataToRender = dataCols;
+// let displayMode: string = '00default'; // eq "00default" or `benchName`
+let absoluteDisplayMode = true;
 
 console.log(dataCols);
 
 setTimeout(() => {
-    c3.generate({
+    var chartApi = c3.generate({
         title: {
             text: colNameToDisplayName[dataColName],
         },
         data: {
             x: 'x',
-            columns: dataCols,
+            columns: dataToRender,
             type: 'bar',
             order: null,
         },
@@ -77,7 +108,49 @@ setTimeout(() => {
             },
         },
         bindto: chart,
+        zoom: {
+            type: 'drag',
+        },
+        legend: {
+            item: {
+                onclick: (id) => {
+                    console.log(id);
+                    console.log(benchMeanTimes[id]);
+                    absoluteDisplayMode = !absoluteDisplayMode;
+                    const renderData = absoluteDisplayMode
+                        ? dataCols
+                        : buildDataCols();
+
+                    chartApi.load({
+                        data: {
+                            keys: dataCols.map((arr) => arr[0]),
+                            rows: (() => {
+                                const entries = renderData.map((arr) =>
+                                    arr
+                                        .slice(1)
+                                        .map((dataEl) => [arr[0], dataEl])
+                                );
+                                console.log('entries', entries);
+                                const remapped = [];
+
+                                for (let i = 0; i < entries[0].length; i++) {
+                                    const rowThing = [];
+                                    for (let j = 0; j < entries.length; j++) {
+                                        rowThing.push(entries[j][i]);
+                                    }
+                                    remapped.push(rowThing);
+                                }
+                                console.log('remapped', remapped);
+                                return remapped.map(Object.fromEntries);
+                            })(),
+                        },
+                    });
+                },
+            },
+        },
     });
+
+    // chartApi.zoom.enable(true);
 
     // groups are by size (variable)
     // bars are by collection type (benchmark name/id)
